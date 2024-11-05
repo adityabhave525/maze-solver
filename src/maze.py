807359c93d857bd1108c59a3de4556a189598179
adityabhave525 <adityabhave525@gmail.com>
2024-11-05 11:03:08 +0000

    Maze is half done

--- src/maze.py
+++ src/maze.py
@@ -22,9 +22,10 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        
+
         self._create_cells()
-    
+        self._break_entrance_and_exit()
+
     def _create_cells(self):
         for i in range(self._num_cols):
             col_cells = []
@@ -34,19 +35,25 @@ class Maze:
         for i in range(self._num_cols):
             for j in range(self._num_rows):
                 self._draw_cell(i, j)
-    
+
     def _draw_cell(self, i, j):
         if self._win is None:
             return
         x1 = self._x1 + i * self._cell_size_x
-        y1 = self._y1 + i * self._cell_size_y
+        y1 = self._y1 + j * self._cell_size_y
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
         self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
-        
+
     def _animate(self):
         if self._win is None:
             return
         self._win.redraw()
         time.sleep(0.05)
+
+    def _break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._draw_cell(0, 0)
+        self._cells[self._num_cols - 1][self._num_rows - 1].has_bottom_wall = False
+        self._draw_cell(self._num_cols - 1, self._num_rows - 1)
