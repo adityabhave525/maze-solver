59611da0299855940a13ea3c8d04a2cf5bb8bd9f
adityabhave525 <adityabhave525@gmail.com>
2024-11-05 07:48:08 +0000

    Line can be drawn on the window

--- src/graphics.py
+++ src/graphics.py
@@ -2,23 +2,41 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root = Tk()
         self.__root.title = "Maze Generator"
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack()
+        self.__running = False
     
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
     
     def wait_for_close(self):
-        self.running = True
-        while self.running:
+        self.__running = True
+        while self.__running:
             self.redraw()
-        
+        print("Window Closed")
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.__canvas, fill_color)
+    
     def close(self):
-        self.running = False
+        self.__running = False
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
